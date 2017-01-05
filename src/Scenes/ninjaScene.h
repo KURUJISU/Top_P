59fa89153362a7dcc2d2f06a4dedec6328a33b02
Naoto Fujiwara <asyura1327@yahoo.co.jp>
2017-01-05 21:15:00 +0900

    scene up

---
+++ src/Scenes/ninjaScene.h
@@ -0,0 +1,30 @@
+﻿
+/**
+ * @file   ninjaScene.h
+ * @brief  テストシーン
+ *
+ * @author y.akira
+ * @date   2016.12.21
+ */
+
+#pragma once
+
+
+//! @brief テスト用シーンクラス
+class NinjaScene : public ofxScene {
+private:
+	yCamera     cam_;
+	BackGround  bg_;
+
+	shared_ptr<Player> player_;
+
+	void moveCam();
+	float offsetY_;
+public:
+	void setup() override;
+	void update(float deltaTime) override;
+	void draw() override;
+	void gui() override;
+
+	void keyPressed(int key) override;
+};
